e55360241a69a6173eb5418743f40088a4d0758e
Batul <albatularts@gmail.com>
2024-07-06 12:56:45 +0300

    test

---
+++ js/aboutme.js
@@ -0,0 +1,44 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const buttons = document.querySelectorAll('.about-buttons .button');
+    const infoText = document.getElementById('info-text');
+
+    buttons.forEach(button => {
+        button.addEventListener('mouseover', () => {
+            const isActive = button.classList.contains('active');
+
+            if (!isActive) {
+                buttons.forEach(b => {
+                    if (b !== button) {
+                        b.style.opacity = '0';
+                        b.disabled = true;
+                    }
+                });
+                button.classList.add('active');
+                infoText.innerHTML = button.getAttribute('data-text');
+                infoText.style.display = 'block';
+                setTimeout(() => {
+                    infoText.style.opacity = '1';
+                }, 30);
+            }
+        });
+
+        button.addEventListener('mouseleave', () => {
+            buttons.forEach(b => {
+                b.classList.remove('active');
+                b.style.opacity = '1';
+                b.disabled = false;
+            });
+            infoText.style.opacity = '0';
+            setTimeout(() => {
+                infoText.style.display = 'none';
+            }, 30);
+        });
+    });
+});
+
+
+
+
+
+
+
